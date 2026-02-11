68f2a8c74477c2bb54edad83c1e235431acb1053
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.6>
2026-02-11 13:13:44 -0300

    feat: Testing data generated with Bogus

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CommonTestUtilities.Requests;
 
 namespace Validators.Tests.Expenses.Register
 {
@@ -10,15 +11,7 @@ namespace Validators.Tests.Expenses.Register
         {
             //Arrange
             var validator = new RegisterExpenseValidator();
-            var request = new RequestRegisterExpenseJson
-            {
-                Amount = 100,
-                Date = DateTime.Now.AddDays(-1),
-                Description = "Description",
-                Title = "Apple",
-                PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
-
-            };
+            var request = RequestRegisterExpenseJsonBuilder.Build();
 
             //Act
             var result = validator.Validate(request);
