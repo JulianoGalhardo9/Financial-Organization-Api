0844213adfa0baf1951bb206ce319365963fdb3f
Juliano Galhardo de Oliveira <julianogalhardo@MacBook-Air-de-Juliano.local>
2026-02-25 15:03:53 -0300

    feat: Creating a page

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -26,16 +26,38 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
                 return Array.Empty<byte>(); ;
             }
 
+            var document = CreateDocument(month);
+            var page = CreatePage(document);
 
-
-            return Array.Empty<byte>(); ;
+            return Array.Empty<byte>();
         }
 
         private Document CreateDocument(DateOnly month)
         {
             var document = new Document();
-            document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR}";
+
+            document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR} {month:Y}";
+            document.Info.Author = "Juliano Galhardo";
+
+            var style = document.Styles["Normal"];
+            style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+
             return document;
+        }
+
+        private Section CreatePage(Document document)
+        {
+            var section = document.AddSection();
+            section.PageSetup = document.DefaultPageSetup.Clone();
+
+            section.PageSetup.PageFormat = PageFormat.A4;
+            section.PageSetup.LeftMargin = 40;
+            section.PageSetup.RightMargin = 40;
+            section.PageSetup.TopMargin = 80;
+            section.PageSetup.BottomMargin = 80;
+
+
+            return section;
         }
     }
 }
