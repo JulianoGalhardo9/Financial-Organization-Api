8cfd412ddb3d403e2b820b3e243f668b9ed2193a
Juliano Galhardo de Oliveira <julianogalhardo@MacBook-Air-de-Juliano.local>
2026-02-26 22:28:21 -0300

    feat: Adjusting the header style

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -32,16 +32,19 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
 
             var table = page.AddTable();
             table.AddColumn();
-            table.AddColumn();
+            table.AddColumn("300");
 
             var row = table.AddRow();
             row.Cells[0].AddImage("/Users/julianogalhardo/Downloads/Foto.jpeg");
 
             row.Cells[1].AddParagraph("Olá, Juliano Galhardo");
             row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
+            row.Cells[1].VerticalAlignment = MigraDoc.DocumentObjectModel.Tables.VerticalAlignment.Center;
 
 
             var paragraph = page.AddParagraph();
+            paragraph.Format.SpaceBefore = "40";
+            paragraph.Format.SpaceAfter = "40";
             var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
 
             paragraph.AddFormattedText(title, new Font { Name = FontHelper.RALEWAY_REGULAR, Size = 15 });
