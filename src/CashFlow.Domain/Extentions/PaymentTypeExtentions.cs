9ba8a61ec6ae09b73140a349c9582cf2298a8872
Juliano Galhardo de Oliveira <julianogalhardo@MacBook-Air-de-Juliano.local>
2026-02-27 00:36:55 -0300

    feat: Adicionando tipo de pagamento

---
+++ src/CashFlow.Domain/Extentions/PaymentTypeExtentions.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Extentions
+{
+	public static class PaymentTypeExtentions
+	{
+		public static string PaymentTypeToString(this PaymentType paymentType)
+		{
+			return paymentType switch
+			{
+				PaymentType.Cash => "Dinheiro",
+				PaymentType.CreditCard => "Cartão de Débito",
+				PaymentType.DebitCard => "Cartão de Crédito",
+				PaymentType.EletronicTransfer => throw new NotImplementedException()
+			};
+		}
+	}
+}
+
